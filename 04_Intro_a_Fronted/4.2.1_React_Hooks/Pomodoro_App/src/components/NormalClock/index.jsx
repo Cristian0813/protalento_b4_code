9b04c5cf1d355d777baaf027a9a6a90c971770e3
Cristian A <javier.0813@hotmail.com>
2023-04-26 00:41:13 -0500

    Intro a React

---
+++ 04_Intro_a_Fronted/4.2.1_React_Hooks/Pomodoro_App/src/components/NormalClock/index.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import './styles.css';
+
+/*
+Los hooks son funciones que permiten "engancharse" a los componentes de 
+React para acceder a ciertas funcionalidades, como el estado o el ciclo 
+de vida del componente, desde componentes funcionales. 
+*/
+
+    function NormalClock(){
+
+    const [hour, setHour] = useState('00')
+    const [minutes, setMinutes] = useState('00')
+    const [seconds, setSeconds] = useState('00')
+
+    useEffect(() => {
+        const interval = setInterval(()=>{
+            const date = new Date()
+            const h = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
+            const m = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+            const s = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
+            setHour(h)
+            setMinutes(m)
+            setSeconds(s)
+        }, 1000)
+
+        /*
+        Retorna una función que se ejecutará cuando se desmonte el componente o cuando 
+        cambien las dependencias especificadas. Esta función limpia el intervalo 
+        creado con setInterval utilizando la función clearInterval, evitando que la 
+        función de callback se siga ejecutando después de desmontar el componente o 
+        actualizar las dependencias.
+        */
+
+        return () => clearInterval(interval)
+    }, [hour, minutes, seconds]);
+
+    return(
+        <div className='clock'>
+            <div className='progress-go' />
+            <p className='status'>Reloj</p>
+            <span>
+                <span className='hour'>{hour}:</span>
+                <span className='minute'>{minutes}:</span>
+                <span className='minute'>{seconds}</span>
+            </span>
+        </div>
+    )
+}
+
+export default NormalClock
